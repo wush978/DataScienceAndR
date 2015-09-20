2e0d9008ed8ca78df7e07307dc26c54b57ab57fa
Wush Wu <wush978@gmail.com>
2015-09-20 22:10:32 +0800

    intro of DataTaipei + ggmap + kriging

--- DataScienceAndR/RBasic/customTests.R
+++ DataScienceAndR/RBasic/customTests.R
@@ -4,3 +4,10 @@ test_package_version <- function(pkg_name, pkg_version) {
     packageVersion(pkg_name) >= package_version(pkg_version),
     error = function(e) FALSE)
 }
+
+test_search_path <- function(pkg_name) {
+  tryCatch(
+    length(grep(sprintf("/%s$", pkg_name), searchpaths())) > 0,
+    error = function(e) FALSE)
+}
+
