79eca47691ac155b53571cb5fc012c31d95800b3
Wush Wu <wush978@gmail.com>
2015-09-21 15:04:26 +0800

    install_github

--- DataScienceAndR/RBasic-Motivation/initLesson.R
+++ DataScienceAndR/RBasic-Motivation/initLesson.R
@@ -3,3 +3,9 @@ check_then_install <- function(pkg_name, pkg_version) {
   if (packageVersion(pkg_name) < package_version(pkg_version)) utils::install.packages(pkg_name, repos = "http://cran.r-project.org")
   }
 }
+
+check_then_install_github <- function(pkg_name, pkg_version, ...) {
+  if (!require(pkg_name)) devtools::install_github(...) else {
+  if (packageVersion(pkg_name) < package_version(pkg_version)) devtools::install_github(...)
+  }
+}
