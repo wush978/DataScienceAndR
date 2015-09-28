1143da87cafe73d3d803c4ffcdb065e63e3dc034
ChihChengLiang <chihchengliang@gmail.com>
2015-09-28 20:25:54 +0800

    first commit

---
+++ RBasic-Factors/initLesson.R
@@ -0,0 +1,10 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+blood_type <- c("B","A","A","O","AB","O",
+                "A","AB","O","AB","O","AB",
+                "A","A","A","AB","A","AB",
+                "AB","A","AB","A","AB","B",
+                "A","O","A","A","AB","AB")
