8c3cde47fa2b74994f6e023ef8a377d069c12da7
Wush Wu <wush978@gmail.com>
2015-09-29 17:58:59 +0800

    init r-basic-object

---
+++ RBasic-03-Data-Structure-Object/initLesson.R
@@ -0,0 +1,8 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+assign("x", c(1,2,3,4,5))
+
+assign("g", lm(dist ~ speed, cars))
