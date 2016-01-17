0f517c9a6fe324d799d7587b3cb2331f6e18d811
Wush Wu <wush978@gmail.com>
2016-01-17 23:33:02 +0800

    use l10n_info to handle strange bug of read.table

--- .initCourse.R
+++ .initCourse.R
@@ -3,4 +3,11 @@
   normalizePath(path, mustWork = TRUE)
 }
 
-
+.read.table.big5 <- function(file, header = FALSE, sep = "", ...) {
+  info <- l10n_info()
+  if (info$MBCS & (!info$`UTF-8`)) {
+    read.table(file, header = header, sep = sep, ...) 
+  } else {
+    read.table(file(file, encoding = "BIG5"), header = header, sep = sep, ...) 
+  }
+}
