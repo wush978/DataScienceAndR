7efb4f43e5bd5dd51578acfe2d568001330dbef9
Wush Wu <wush978@gmail.com>
2016-01-18 01:12:21 +0800

    init rdataengineer-02

---
+++ RDataEngineer-02-XML/initLesson.R
@@ -0,0 +1,20 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+assign("tender_path",
+        .get_path("0080400004.html"),
+        envir = globalenv())
+
+wiki_dom <- function() {
+  browseURL("https://zh.wikipedia.org/wiki/%E6%96%87%E6%A1%A3%E5%AF%B9%E8%B1%A1%E6%A8%A1%E5%9E%8B")
+}
+
+wiki_html <- function() {
+  browseURL("https://zh.wikipedia.org/wiki/HTML")
+}
+
+wiki_xpath <- function() {
+  browseURL("https://zh.wikipedia.org/wiki/XPath")
+}
