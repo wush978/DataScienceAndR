66a3cfc48019e58fc50e8db882671d4d5bb59901
Wush Wu <wush978@gmail.com>
2016-01-17 21:56:52 +0800

    add custom tests

--- RDataEngineer-01-Parsing/customTests.R
+++ RDataEngineer-01-Parsing/customTests.R
@@ -10,3 +10,43 @@
 # variables when appropriate. The answer test, creates_new_var()
 # can be used for for the purpose, but it also re-evaluates the
 # expression which the user entered, so care must be taken.
+
+rdataengineer_01_hw_test <- function() {
+  e <- get("e", parent.frame())
+  source_result <- try(source(e$script_temp_path, local = new.env(), encoding = "UTF-8"), silent = TRUE)
+  if (class(source_result)[1] == "try-error") return(FALSE)
+  name.list <- c("pirate_info", "pirate_info_key_value", "pirate_info_key", "pirate_is_coordinate", 
+                 "pirate_coordinate_raw", "pirate_coordinate_latitude", "pirate_df")
+  pirate_info.ref <- readLines(file(pirate_path, encoding = "BIG5"))
+  pirate_info_key_value.ref <- strsplit(pirate_info.ref, "：")
+  pirate_info_key.ref <- sapply(pirate_info_key_value.ref, "[", 1)
+  pirate_is_coordinate.ref <- pirate_info_key.ref == "經緯度"
+  pirate_coordinate_raw.ref <- local({
+    .tmp <- sapply(pirate_info_key_value.ref, "[", 2)
+    .tmp[pirate_is_coordinate.ref]
+  })
+  pirate_coordinate_latitude.ref <- {
+    as.integer(substring(pirate_coordinate_raw.ref, 3, 4))
+  }
+  pirate_coordinate_longitude.ref <- {
+    # 請在這邊填寫你的程式碼
+    # 這個程式碼可以多行
+    as.integer(substring(pirate_coordinate_raw.ref, 12, 14))
+  }
+  pirate_df.ref <- data.frame(
+    latitude = pirate_coordinate_latitude.ref,
+    longitude = pirate_coordinate_longitude.ref
+  )
+  tryCatch({
+    for(name in name.list) {
+      if (!isTRUE(all.equal(
+        get(name, envir = globalenv()),
+        get(sprintf("%s.ref", name))
+      ))) stop(sprintf("%s is wrong! Try again.\n", name))
+    }
+    TRUE
+  }, error = function(e) {
+    cat(conditionMessage(e))
+    FALSE
+  })
+}
