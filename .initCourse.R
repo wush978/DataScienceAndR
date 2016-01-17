ff9a7c6eb84e8b233b472d4af464b6c6a4ca8e1e
Wush Wu <wush978@gmail.com>
2016-01-18 01:11:58 +0800

    add package tester and checker to .initCourse

--- .initCourse.R
+++ .initCourse.R
@@ -18,3 +18,16 @@ check_then_install <- function(pkg_name, pkg_version) {
   }
 }
 
+test_package_version <- function(pkg_name, pkg_version) {
+  e <- get("e", parent.frame())
+  tryCatch(
+    packageVersion(pkg_name) >= package_version(pkg_version),
+    error = function(e) FALSE)
+}
+
+test_search_path <- function(pkg_name) {
+  tryCatch(
+    length(grep(sprintf("/%s$", pkg_name), searchpaths())) > 0,
+    error = function(e) FALSE)
+}
+
