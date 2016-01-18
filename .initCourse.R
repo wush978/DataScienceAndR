bbc713f317b6fb7f8045f874b88dc7874bb92933
Wush Wu <wush978@gmail.com>
2016-01-18 22:12:36 +0800

    fix #159

--- .initCourse.R
+++ .initCourse.R
@@ -3,11 +3,13 @@
   normalizePath(path, mustWork = TRUE)
 }
 
-.read.table.big5 <- function(file, header = FALSE, sep = "", ...) {
-  info <- l10n_info()
-  if ("codepage" %in% names(info)) {
-    read.table(file, header = header, sep = sep, ...) 
-  } else {
-    read.table(file(file, encoding = "BIG5"), header = header, sep = sep, ...) 
-  }
-}
+assign(".read.table.big5", 
+       function(file, header = FALSE, sep = "", ...) {
+         info <- l10n_info()
+         if ("codepage" %in% names(info)) {
+           read.table(file, header = header, sep = sep, ...) 
+         } else {
+           read.table(file(file, encoding = "BIG5"), header = header, sep = sep, ...) 
+         }
+       },
+       envir = globalenv())
