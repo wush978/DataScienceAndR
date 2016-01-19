60879b7c06ec4ab52067788da10b1b82bf8949b3
Wush Wu <wush978@gmail.com>
2016-01-20 02:11:55 +0800

    pipeline operator

--- RDataEngineer-05-Data-Manipulation/customTests.R
+++ RDataEngineer-05-Data-Manipulation/customTests.R
@@ -14,3 +14,27 @@
 test_equal <- function(a, b) {
   isTRUE(all.equal(a, b))
 }
+
+rdataengineer_05_04 <- function(answer04.1, answer04.2, answer04.3) {
+  if (answer04.1 !=
+    filter(flights, month == 1) %>%
+    mutate(gain = arr_delay - dep_delay) %>%
+    summarise(mean(gain, na.rm = TRUE)) %>%
+    `[[`(1)) {
+      message("answer04.1 is wrong")
+      return(FALSE)
+    }
+  if (answer04.2 != FALSE) {
+    message("answer04.2 is wrong")
+    return(FALSE)
+  }
+  if (answer04.3 !=
+    filter(flights, 2301 <= dep_time, dep_time <= 2400) %>%
+    summarise(mean(dep_delay, na.rm = TRUE)) %>%
+    `[[`(1)) {
+      message("answer04.3 is wrong")
+      return(FALSE)
+    }
+  TRUE
+
+}
