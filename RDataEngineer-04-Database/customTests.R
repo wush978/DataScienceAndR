b6af8bbcbc0b1f31112c912cad69ad3694599b3f
Wush Wu <wush978@gmail.com>
2016-01-19 14:07:53 +0800

    update tests

--- RDataEngineer-04-Database/customTests.R
+++ RDataEngineer-04-Database/customTests.R
@@ -12,14 +12,17 @@
 # expression which the user entered, so care must be taken.
 
 check_lvr_land <- function(db) {
-  .tmp1 <- dbReadTable(db, "lvr_land")
+  convert <- function(x, encoding) {
+    Encoding(x) <- encoding
+    enc2native(x)
+  }
+  converter <- function(x, encoding) {
+    if (is.character(x)) convert(x, encoding) else x
+  }
+  .tmp1 <- dbReadTable(db, "lvr_land2")
   .tmp2 <- readRDS(.get_path("lvr_land_read.Rds"))
-  .tmp1.1 <- lapply(.tmp1, function(x) x)
-  .tmp2.1 <- lapply(.tmp2, function(x) {
-    if (is.character(x)) {
-      Encoding(x) <- "UTF-8"
-      enc2native(x)
-    } else x
-    })
+  .tmp1.1 <- lapply(.tmp1, converter, "BIG5")
+  .tmp2.1 <- lapply(.tmp2, converter, "UTF-8")
+  names(.tmp1.1) <- names(.tmp2.1) <- NULL
   isTRUE(all.equal(.tmp1.1, .tmp2.1))
 }
