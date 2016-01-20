88212007aaf1b7375d097c202b0c8f0641f94366
Wush Wu <wush978@gmail.com>
2016-01-20 10:46:20 +0800

    init 06, got 01

--- RDataEngineer-06-Join/customTests.R
+++ RDataEngineer-06-Join/customTests.R
@@ -10,3 +10,17 @@
 # variables when appropriate. The answer test, creates_new_var()
 # can be used for for the purpose, but it also re-evaluates the
 # expression which the user entered, so care must be taken.
+
+rdataengineer_06_01 <- function(answer01) {
+  answer01.ref <- local({
+    # 請在此填寫你的程式碼
+    slice(flights, 1:100) %>%
+      select(year:day, tailnum, carrier) %>%
+      left_join(y = airlines, by = "carrier")
+  })
+  retval <- all.equal(data.frame(answer01), data.frame(answer01.ref))
+  if (!isTRUE(retval)) {
+    message(retval)
+    FALSE
+  } else TRUE
+}
