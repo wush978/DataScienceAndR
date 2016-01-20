ba022b249354e9d2e785103385db8b09476c2201
Wush Wu <wush978@gmail.com>
2016-01-20 12:00:28 +0800

    rdataengineer 06-02

--- RDataEngineer-06-Join/customTests.R
+++ RDataEngineer-06-Join/customTests.R
@@ -12,6 +12,9 @@
 # expression which the user entered, so care must be taken.
 
 rdataengineer_06_01 <- function(answer01) {
+  e <- get("e", parent.frame())
+  source_result <- try(source(e$script_temp_path, local = new.env(), encoding = "UTF-8"), silent = TRUE)
+  if (class(source_result)[1] == "try-error") return(FALSE)
   answer01.ref <- local({
     # 請在此填寫你的程式碼
     slice(flights, 1:100) %>%
@@ -23,4 +26,40 @@ rdataengineer_06_01 <- function(answer01) {
     message(retval)
     FALSE
   } else TRUE
+}
+
+rdataengineer_06_02 <- function(answer02.1, answer02.2, answer02.3) {
+  e <- get("e", parent.frame())
+  source_result <- try(source(e$script_temp_path, local = new.env(), encoding = "UTF-8"), silent = TRUE)
+  if (class(source_result)[1] == "try-error") return(FALSE)
+  check_equal <- function(a, b, msg) {
+    retval <- all.equal(a, b)
+    if (!isTRUE(retval)) {
+      message(msg)
+      message(retval)
+      FALSE
+    } else TRUE
+  }
+  answer02.1.ref <- local({
+    select(flights, year:day, hour, origin, dest, tailnum, carrier, arr_delay) %>%
+      left_join(weather) %>%
+      select(wind_speed, arr_delay) %>%
+      filter(!is.na(wind_speed), !is.na(arr_delay))
+  })
+  if (!check_equal(answer02.1, answer02.1.ref, "answer02.1 is wrong")) {
+    return(FALSE)
+  }
+  answer02.2.ref <- quantile(answer02.1.ref$wind_speed, seq(0, 1, by = 0.25))
+  if (!check_equal(answer02.2, answer02.2.ref, "answer02.2 is wrong")) {
+    return(FALSE)
+  }
+  answer02.3.ref <- local({
+    mutate(answer02.1.ref, wind_speed = cut(wind_speed, breaks = c(-Inf, answer02.2.ref))) %>%
+      group_by(wind_speed) %>%
+      summarise(mean(arr_delay))
+  })
+  if (!check_equal(answer02.3, answer02.3.ref, "answer02.3 is wrong")) {
+    return(FALSE)
+  }
+  TRUE
 }
