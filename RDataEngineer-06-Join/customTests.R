5f9fef54d710b6ee9029071b7acfbc119050610b
Wush Wu <wush978@gmail.com>
2016-01-20 15:38:33 +0800

    finish 06 join

--- RDataEngineer-06-Join/customTests.R
+++ RDataEngineer-06-Join/customTests.R
@@ -64,6 +64,69 @@ rdataengineer_06_02 <- function(answer02.1, answer02.2, answer02.3) {
   TRUE
 }
 
-rdataengineer_06_hw <- function() {
-  TRUE
+
+rdataengineer_06_hw <- function(gdp, cl_info_year, answerHW) {
+  e <- get("e", parent.frame())
+  source_result <- try(source(e$script_temp_path, local = new.env(), encoding = "UTF-8"), silent = TRUE)
+  if (class(source_result)[1] == "try-error") return(FALSE)
+  tryCatch({
+    stopifnot(is.data.frame(gdp))
+    stopifnot(colnames(gdp) == c("year", "gdp"))
+    stopifnot(class(gdp$year) == "character")
+    stopifnot(class(gdp$gdp) == "numeric")
+    stopifnot(nrow(gdp) == 33)
+    stopifnot(range(gdp$year) == c("1981", "2013"))
+    stopifnot(range(gdp$gdp) == c(1810829,14564242) * 1000000)
+    stopifnot(is.data.frame(cl_info_year))
+    stopifnot(colnames(cl_info_year) == c("year", "mortgage_total_bal"))
+    stopifnot(class(cl_info_year$year) == "character")
+    stopifnot(class(cl_info_year$mortgage_total_bal) == "numeric")
+    stopifnot(nrow(cl_info_year) == 9)
+    stopifnot(range(cl_info_year$year) == c("2006", "2014"))
+    stopifnot(range(cl_info_year$mortgage_total_bal) == c(3.79632e+12, 5.726784e+12))
+    stopifnot(is.data.frame(answerHW))
+    stopifnot(nrow(answerHW) == 8)
+    stopifnot(colnames(answerHW) == c("year", "index"))
+    stopifnot(class(answerHW$year) == "character")
+    stopifnot(class(answerHW$index) == "numeric")
+    stopifnot(min(answerHW$index) > 0.3)
+    stopifnot(max(answerHW$index) < 0.4)
+    answerHW.ref <- local({
+      gdp <- local({
+        # 請填寫你的程式碼
+        read.table(gdp_path, skip = 4, header = FALSE, sep = ",") %>%
+          slice(1:132) %>%
+          select(season = V1, gdp = V2) %>%
+          mutate(
+            season = as.character(season),
+            year = substring(season, 1, 4),
+            gdp = gsub(pattern = ",", replacement = "", x = gdp), 
+            gdp = as.numeric(gdp) * 1000000) %>%
+          group_by(year) %>%
+          summarise(gdp = sum(gdp))
+      })
+      cl_info_year <- local({
+        select(cl_info, data_dt, mortgage_bal) %>%
+          mutate(month = substring(data_dt,1,7)) %>%
+          group_by(month) %>%
+          summarise(mortgage_total_bal = sum(mortgage_bal, na.rm = TRUE)) %>%
+          mutate(year = substring(month, 1, 4)) %>%
+          group_by(year) %>%
+          arrange(month) %>%
+          summarise(month = head(month, 1), mortgage_total_bal = head(mortgage_total_bal, 1)) %>%
+          select(year, mortgage_total_bal)
+      })
+      local({
+        # 請在這邊填寫你的程式碼
+        inner_join(cl_info_year, gdp, by = "year") %>%
+          mutate(index = mortgage_total_bal / gdp) %>%
+          select(year, index)
+      })
+    })
+    stopifnot(sum(answerHW.ref$index) == sum(answerHW$index))
+    TRUE
+  }, error = function(e) {
+    message(conditionMessage(e))
+    FALSE
+  })
 }
