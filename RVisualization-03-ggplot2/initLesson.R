42ff07fd37794e21609de4890fb5da90c14429bd
Wush Wu <wush978@gmail.com>
2016-01-27 19:15:24 +0800

    assing variables to globalenv()

--- RVisualization-03-ggplot2/initLesson.R
+++ RVisualization-03-ggplot2/initLesson.R
@@ -2,9 +2,9 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
-hsb <- read.csv(.get_path("hsb.csv"), header = TRUE)
+assign("hsb", read.csv(.get_path("hsb.csv"), header = TRUE), envir = globalenv())
 .population.path <- tempfile(fileext = ".csv")
-population <- local({
+assign("population", local({
   cat("Loading data from opendata10412M030.csv...\n")
   population.src <- readLines(file(.get_path("opendata10412M030.csv"), encoding = "UTF-8"))
   write(population.src[-2], .population.path)
@@ -27,16 +27,16 @@ population <- local({
     do.call(rbind, tmp2)
   })
   do.call(rbind, tmp)
-})
+}), envir = globalenv())
 invisible(gc())
-population_src <- function() {
+assign("population_src", function() {
   browseURL("http://data.moi.gov.tw/MoiOD/Data/DataDetail.aspx?oid=F4478CE5-7A72-4B14-B91A-F4701758328F")
-}
+}, envir = globalenv())
 suppressWarnings(try(rm(list = ".start_rvisualization_03", envir = globalenv()), silent = TRUE))
 .hw_description <- readLines(file(.get_path(".hw_description.txt"), encoding = "UTF-8"))
 
-begin_rvisualization_03 <- function() { }
-hw_01 <- function() {
+assign("begin_rvisualization_03", function() { }, envir = globalenv())
+assign("hw_01", function() {
   g <-
     group_by(population, age) %>%
     summarise(count = sum(count)) %>%
@@ -45,9 +45,9 @@ hw_01 <- function() {
     geom_point() + geom_line()
   print(g)
   invisible(NULL)
-}
+}, envir = globalenv())
 
-hw_02 <- function() {
+assign("hw_02", function() {
   cat(.hw_description[1]);cat("\n")
   g <-
     filter(population, site_id == .hw_description[2], village == .hw_description[5]) %>%
@@ -56,13 +56,13 @@ hw_02 <- function() {
     ggplot(aes(x = village, y = count, fill = sex)) +
     geom_bar(position = "dodge", stat = "identity")
   print(g)
-}
+}, envir = globalenv())
 
-hw_03 <- function() {
+assign("hw_03", function() {
   cat(.hw_description[3]);cat("\n")
   g <-
     filter(population, site_id == .hw_description[4], village == .hw_description[5]) %>%
     ggplot(aes(x = age, y = count, color = sex)) +
     geom_point() + geom_line()
   print(g)
-}
+}, envir = globalenv())
