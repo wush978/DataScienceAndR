36e30cfd6371470b33027f0a872cebb01a9a4379
Wush Wu <wush978@gmail.com>
2016-01-28 02:16:51 +0800

    debug: invalid multibyte character

--- RVisualization-04-Javascript-And-Maps/initLesson.R
+++ RVisualization-04-Javascript-And-Maps/initLesson.R
@@ -25,7 +25,7 @@ assign("hw", function() {
   src <- readLines(file(pirate_path, encoding = "BIG5"))
   tmp <- strsplit(src, "：")
   key <- sapply(tmp, "[[", 1)
-  is_target <- key == "經緯度"
+  is_target <- key == readLines(.get_path(".hw")[1])
   value <- sapply(tmp[is_target], "[[", 2)
   pirate <- data.frame(
     lat = substring(value, 3, 4) %>% as.numeric + substring(value, 6, 7) %>% as.numeric / 60,
