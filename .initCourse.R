21609359a92cc8322c42cf08a1735d21ace2bd13
Wush Wu <wush978@gmail.com>
2016-02-17 20:08:38 +0800

    suppress messages from testing existence of the packages

--- .initCourse.R
+++ .initCourse.R
@@ -7,7 +7,7 @@ assign(".get_path",
 
 assign("check_then_install",
        function(pkg_name, pkg_version) {
-         if (!require(pkg_name, character.only = TRUE)) utils::install.packages(pkg_name, repos = "http://cran.r-project.org") else {
+         if (!suppressWarnings(suppressMessages(require(pkg_name, character.only = TRUE)))) utils::install.packages(pkg_name, repos = "http://cran.r-project.org") else {
            if (packageVersion(pkg_name) < package_version(pkg_version)) utils::install.packages(pkg_name, repos = "http://cran.r-project.org")
          }
        },
