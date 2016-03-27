077a65b88c8913d51a4f630927a77d3241df8706
aha <ntuaha@gmail.com>
2016-03-27 15:28:22 +0800

    增加開發語言

---
+++ Project-ROpenData-Four-Asian-Tigers/prepare.R
@@ -0,0 +1,31 @@
+source("http://wush978.github.io/R/init-swirl.R")
+library(swirl);swirl()
+
+library(swirlify)
+new_lesson("Demo",".")
+set_lesson("Demo/lesson.yaml")
+setwd("./DataScienceAndR/Project-ROpenData-Four-Asian-Tigers/lesson.yaml")
+source(".tranform.R")
+testit()
+#Rscript .test_course.R push full test
+#Rscript .test_course.R commit partial test
+
+library(swirl)
+uninstall_all_courses()
+1
+#setwd(paste(getwd(),"/../",sep=""))
+#setwd("./github/")
+install_course_directory("./")
+#install_course_github("TaiwanRUserGroup",course_name = "DataScienceAndR",branch = "course")
+
+swirl()
+aha
+1
+24
+2
+4
+0
+0
+19
+
+
